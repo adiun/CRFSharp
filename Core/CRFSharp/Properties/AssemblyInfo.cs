201c4d847a5faae98eef9442099560dd55b5e04f
Adi Unnithan <adunnith@microsoft.com>
2020-01-15 23:21:55 -0800

    updates

--- Core/CRFSharp/Properties/AssemblyInfo.cs
+++ Core/CRFSharp/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CRFSharp")]
 [assembly: AssemblyDescription("CRFSharp core algorithm APIs")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zhongkai Fu")]
-[assembly: AssemblyProduct("CRFSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("08e14d97-dd83-4873-959c-1a190fa4e197")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
